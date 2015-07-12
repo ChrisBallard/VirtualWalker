d64175043db0fa7200c87a0fca6aee29a9a6c67f
Chris Ballard <worldoflard@googlemail.com>
2015-07-12 16:11:47 +0000

    add secrets template file

---
+++ secrets.api-keys.template.js
@@ -0,0 +1,15 @@
+(function (apiKeys) {
+    
+    apiKeys.google = "";
+    apiKeys.sessionSecret = "";
+    
+    apiKeys.googleClientId = "";
+    apiKeys.googleClientSecret = "";
+    
+    apiKeys.adminUser = "";
+    apiKeys.adminPassword = "";
+    
+    apiKeys.shortenerDev = "";
+    apiKeys.shortenerProd = "";
+
+})(module.exports);
